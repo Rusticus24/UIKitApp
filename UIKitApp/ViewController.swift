0e9960f2699bcec3f3487b87f29cc57ee00ae548
Ruslan Ivanov <ivanoff0897@gmail.com>
2020-08-15 10:13:52 +0300

    Add launch screen

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Ruslan on 15.08.2020.
+//  Copyright © 2020 Ruslan Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
